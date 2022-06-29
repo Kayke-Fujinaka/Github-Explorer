34062c6ea97b65de20f68c2d73c3512e89d2096a
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2022-06-29 13:18:31 -0300

    [Feat] Applying UseEffect for List

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,5 +1,6 @@
 import { RepositoryItem } from "./RepositoryItem";
 import "../styles/repositories.scss";
+import { useEffect, useState } from "react";
 
 const repository = {
   name: "unform",
@@ -8,6 +9,14 @@ const repository = {
 };
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/Kayke-Fujinaka/repos")
+      .then((response) => response.json()) // Converte a resposta pra JSON
+      .then((data) => setRepositories(data)); // Os dados convertidos definidos no repositories
+  }, []);
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
